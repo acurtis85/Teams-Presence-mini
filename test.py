2b99d07f548b3cf420195bb8e0af7a4eb37cc1ac
acurtis85 <anth0nycurtis85@gmail.com>
2023-02-01 18:22:56 -0500

    Update test.py

--- test.py
+++ test.py
@@ -156,10 +156,10 @@ def plot_points():
 	unicorn.show()
 
 def blinkRandom():
-	#t = threading.currentThread()
-	#while getattr(t, "do_run", True):
-	if len(points) < 10 and randint(0, 5) > 1:
-		points.append(LightPoint())
+	t = threading.currentThread()
+	while getattr(t, "do_run", True):
+		if len(points) < 10 and randint(0, 5) > 1:
+			points.append(LightPoint())
 		plot_points()
 		update_positions()
 		sleep(0.03)
